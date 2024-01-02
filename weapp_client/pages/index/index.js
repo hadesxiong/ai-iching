96bc99cf77f3b10c438a68e4e37b04a50761a327
hadesxiong <fengshixiong890614@gmail.com>
2024-01-02 17:31:27 +0800

    update css files

--- weapp_client/pages/index/index.js
+++ weapp_client/pages/index/index.js
@@ -1,10 +1,18 @@
 Page({
-  data: {},
+  data: {
+    need_show:false,
+  },
+
   onLoad() {
     wx.login({
       success: (res) => {
         console.log(res)
       },
     })
+  },
+  click_liuyao() {
+    let page_obj = this;
+    page_obj.setData({need_show:true});
+    console.log(page_obj.data.need_show)
   }
 })
